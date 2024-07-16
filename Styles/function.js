6197589ba9d0ae773dc0b00710625375bb6ba511
SamDev-Coder <126113961+SamDev-Coder@users.noreply.github.com>
2024-07-16 22:12:34 +0100

    Add files via upload

---
+++ Styles/function.js
@@ -0,0 +1,22 @@
+/*function SayHello(){
+  console.log("Welcome User")
+
+}
+
+function toAddToNumbers(){
+  console.log(1+2)
+}
+
+function Add(a, b, c){
+  return(a+b+c)
+} */
+
+// let answer = Add(3, 4, 15)
+// console.log(`The answer is ${answer}`);
+
+function calculateFahreit(f){
+  return (5/7)*(f-32)
+}
+
+let answer=calculateFahreit(78);
+console.log(answer);
